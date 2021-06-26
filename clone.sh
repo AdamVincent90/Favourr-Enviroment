8e1ee7a61c081ba2f1c5ef76077b92eac0eeabd2
Adam Vincent <adam.vincent@totalprocessing.com>
2021-06-26 16:52:18 +0100

    script change

--- clone.sh
+++ clone.sh
@@ -1,10 +1,9 @@
 echo "Pulling Repositories.."
 git clone https://github.com/AdamVincent90/Favourr-Front.git
-cd Favour-Front
+cd Favourr-Front
 npm install 
 echo "Front Installed"
 cd ..
 git clone https://github.com/AdamVincent90/Favourr-Api-2.git
 cd Favourr-Api-2
-go mod tidy
 go run main.go
