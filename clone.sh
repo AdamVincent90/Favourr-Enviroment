3100879fd579f0571cf2901b4d4b37153ada84a6
Adam Vincent <adam.vincent@totalprocessing.com>
2021-06-27 11:58:04 +0100

    docker changes

--- clone.sh
+++ clone.sh
@@ -2,9 +2,7 @@ echo "Cloning Repositories.."
 git clone https://github.com/AdamVincent90/Favourr-Front.git
 cd Favourr-Front
 npm install 
-npm run serve
 echo "Front Installed"
 cd ..
 git clone https://github.com/AdamVincent90/Favourr-Api-2.git
-cd Favourr-Api-2
-go run main.go
+docker compose up
