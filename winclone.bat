8beee791ce3a3103d6a8d2f994f1c8ea0c34eb75
Adam Vincent <adam.vincent@totalprocessing.com>
2021-06-27 14:13:42 +0100

    git clone script change

--- winclone.bat
+++ winclone.bat
@@ -1,8 +1,8 @@
 echo "Cloning Repositories.."
-git clone git@github.com/AdamVincent90/Favourr-Front.git
+git clone https://github.com/AdamVincent90/Favourr-Front.git
 cd Favourr-Front
 npm install 
 echo "Front Installed"
 cd ..
-git clone git@github.com/AdamVincent90/Favourr-Api-2.git
+git clone https://github.com/AdamVincent90/Favourr-Api-2.git
 docker compose up
